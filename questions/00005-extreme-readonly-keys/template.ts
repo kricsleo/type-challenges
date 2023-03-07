9cc4e03bdc02b11529e158628e6696e9e9f9c8ca
许圣峰 <kricsleo@163.com>
2023-03-07 14:38:54 +0800

    00005

--- questions/00005-extreme-readonly-keys/template.ts
+++ questions/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,8 @@
-type GetReadonlyKeys<T> = any
+type MyEqual<T, K> = 
+  // todo: !! https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-510924206
+  (<G>() => G extends T ? 1 : 2) extends
+  (<G>() => G extends K ? 1 : 2) ? true : false
+
+type GetReadonlyKeys<T> = keyof {
+  [K in keyof T as MyEqual<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? K : never]: any
+}
