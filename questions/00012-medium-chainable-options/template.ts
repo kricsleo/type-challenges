4f8613f7555bade89399fadf99307ca41996bd4b
许圣峰 <kricsleo@163.com>
2023-03-08 13:52:34 +0800

    00012

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,10 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  // todo: !! use extends in function params
+  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<Intersection2Interface<Omit<T, K> & Record<K, V>>>
+  get(): T
+}
+
+// todo: !! use {name: string; b: number} instead of {name:string} & {b:number}
+type Intersection2Interface<T> = {
+  [key in keyof T]: T[key]
 }
