0a6eafd473af468a06f67a5d38672ec4faee5d2e
许圣峰 <kricsleo@163.com>
2023-03-08 11:39:25 +0800

    00009

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,4 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+  // todo: !! use keyof to determine if is an object (vs extends object)
+  readonly [key in keyof T]: keyof T[key] extends never ? T[key] : DeepReadonly<T[key]>
+}
