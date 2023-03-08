54b74337c0a3a2bdc5fab53b681a5a279900d64c
许圣峰 <kricsleo@163.com>
2023-03-08 11:15:22 +0800

    00008

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,7 @@
-type MyReadonly2<T, K> = any
+type MyOmit2<T, K extends keyof T> = {
+  [key in keyof T as key extends K ? never : key]: T[key]
+}
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [key in K]: T[key]
+} & MyOmit2<T, K>
