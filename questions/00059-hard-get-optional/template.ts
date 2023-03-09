43e82243945ec96a73f0853f182a55313fc3da3a
许圣峰 <kricsleo@163.com>
2023-03-09 13:28:57 +0800

    00059

--- questions/00059-hard-get-optional/template.ts
+++ questions/00059-hard-get-optional/template.ts
@@ -1,1 +1,7 @@
-type GetOptional<T> = any
+type MyRequired3<T, K extends keyof T = keyof T> = {
+  [key in K]-?: T[key]
+}
+
+type GetOptional<T> = {
+  [key in keyof T as T[key] extends MyRequired3<T, key>[key] ? never : key]: T[key]
+}
