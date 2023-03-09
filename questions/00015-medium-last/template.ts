be072d2ebf5743b266834e57dca9413b9ed9c982
许圣峰 <kricsleo@163.com>
2023-03-09 10:11:19 +0800

    00015

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,4 @@
-type Last<T extends any[]> = any
+type Last<T extends any[]> = T extends [] 
+  ? never 
+  // todo: !! not allowed in js, but works for ts
+  : T extends [...any, infer last] ? last : never
