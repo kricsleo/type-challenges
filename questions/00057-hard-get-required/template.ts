2b6309bbb8f4dd2e5e661c7a9992a1a089eaac7e
许圣峰 <kricsleo@163.com>
2023-03-09 13:24:55 +0800

    00057

--- questions/00057-hard-get-required/template.ts
+++ questions/00057-hard-get-required/template.ts
@@ -1,1 +1,7 @@
-type GetRequired<T> = any
+type MyRequired2<T, K extends keyof T = keyof T> = {
+  [key in K]-?: T[key]
+}
+
+type GetRequired<T> = {
+  [K in keyof T as T[K] extends MyRequired2<T, K>[K] ? K : never]: T[K]
+}
--- questions/00057-hard-get-required/test-cases.ts
+++ questions/00057-hard-get-required/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+
+type c = GetRequired<{ foo: number; bar?: string }>
 type cases = [
   Expect<Equal<GetRequired<{ foo: number; bar?: string }>, { foo: number }>>,
   Expect<Equal<GetRequired<{ foo: undefined; bar?: undefined }>, { foo: undefined }>>,
