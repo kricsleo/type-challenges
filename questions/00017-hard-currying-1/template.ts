0499a141f3bbaaeaf0e1c9dd227262bb0df7192c
许圣峰 <kricsleo@163.com>
2023-03-09 11:18:35 +0800

    00017

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,6 @@
-declare function Currying(fn: any): any
+declare function Currying<T>(fn: T): 
+  T extends (v1: infer V1, ...rest: infer Rest) => infer R 
+    ? Rest extends [] 
+      ? T
+      : (v: V1) => ReturnType<typeof Currying<(...v: Rest) => R>>
+    : never
--- questions/00017-hard-currying-1/test-cases.ts
+++ questions/00017-hard-currying-1/test-cases.ts
@@ -4,6 +4,9 @@ const curried1 = Currying((a: string, b: number, c: boolean) => true)
 const curried2 = Currying((a: string, b: number, c: boolean, d: boolean, e: boolean, f: string, g: boolean) => true)
 const curried3 = Currying(() => true)
 
+type c = typeof curried1
+type c3 = typeof curried3
+
 type cases = [
   Expect<Equal<
     typeof curried1, (a: string) => (b: number) => (c: boolean) => true
