483ff96d965f085533a73ace6405ab0359ca0c5d
许圣峰 <kricsleo@163.com>
2023-03-09 13:13:44 +0800

    00055

--- questions/00055-hard-union-to-intersection/template.ts
+++ questions/00055-hard-union-to-intersection/template.ts
@@ -1,1 +1,5 @@
-type UnionToIntersection<U> = any
+// !! todo: magic!
+// @see https://fettblog.eu/typescript-union-to-intersection/
+type UnionToIntersection<U> = 
+  (U extends any ? (v: U) => any : never) extends 
+    (v: infer R) => any ? R : never
