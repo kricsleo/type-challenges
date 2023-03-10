0bb8bff6014efcc001b1f9896a20ab09acd0c09a
许圣峰 <kricsleo@163.com>
2023-03-10 13:59:00 +0800

    000730

--- questions/00730-hard-union-to-tuple/template.ts
+++ questions/00730-hard-union-to-tuple/template.ts
@@ -1,1 +1,14 @@
-type UnionToTuple<T> = any
+// todo: use tuple2Union
+type Tuple2Union<T extends any[]> = T[keyof T & number]
+
+// todo: magic!
+// @see https://github.com/type-challenges/type-challenges/issues/21658
+type Union2IntersectionFn<T> = ( T extends any ? (x: () => T) => any : never) extends ((x: infer R) => any) ? R : never
+
+// (() => 'a') & (() => 'b') extends () => infer R ? R : never     =>        'b'
+type LastIntersectionItem<T> = T extends () => infer R ? R : never
+
+type LastUnionItem<T> = LastIntersectionItem<Union2IntersectionFn<T>>
+
+type UnionToTuple<T, K extends any[] = [], Last = LastUnionItem<T>> = 
+  [T] extends [never] ? K : UnionToTuple<Exclude<T, Last>, [...K, Last]>
