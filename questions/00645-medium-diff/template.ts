3e3cab288f40b00c51fd687936c8147de2d5cdec
许圣峰 <kricsleo@163.com>
2023-03-10 10:15:36 +0800

    000645

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,14 @@
-type Diff<O, O1> = any
+// type Diff<O, O1> = Copy<{
+//   [key in keyof O as key extends keyof O1 ? never : key]: O[key]
+// } & {
+//   [key in keyof O1 as key extends keyof O ? never : key]: O1[key]
+// }>
+
+type Copy<T> = {
+  [key in keyof T]: T[key]
+}
+
+// todo: 'keyof (O & O1)' means 'either has key', 'keyof (O | O1)' means 'both have key'
+type Diff<O, O1> = {
+  [key in keyof (O & O1) as key extends keyof (O | O1) ? never : key]: (O & O1)[key]
+}
