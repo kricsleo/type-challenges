3dd90590cd3601db6541e1f8fe4d3b31f56ea371
许圣峰 <kricsleo@163.com>
2023-03-10 16:47:06 +0800

    000734

--- questions/00734-extreme-inclusive-range/template.ts
+++ questions/00734-extreme-inclusive-range/template.ts
@@ -1,1 +1,11 @@
-type InclusiveRange<Lower extends number, Higher extends number> = any
+// todo: construct an array
+type MyTuple<Length, Arr extends number[] = []> = 
+  Arr['length'] extends Length ? Arr : MyTuple<Length, [...Arr, Arr['length']]>
+
+type c = MyTuple<3>
+
+type c2 = [2, 3] extends [1, 2, infer R] ? R : false
+type InclusiveRange<Lower extends number, Higher extends number> = 
+  // todo: array slice
+  MyTuple<Higher> extends [...MyTuple<Lower>, ...infer R] ? [...R, Higher]
+    : []
