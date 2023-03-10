d326296da5b211205980fc24d7b25d265cc3df73
许圣峰 <kricsleo@163.com>
2023-03-10 10:34:34 +0800

    000697

--- questions/00697-extreme-tag/template.ts
+++ questions/00697-extreme-tag/template.ts
@@ -4,6 +4,7 @@ type Tag<B, T extends string> = any
 
 type UnTag<B> = any
 
+// todo: 不会（简单明了）
 type HasTag<B, T extends string> = any
 type HasTags<B, T extends readonly string[]> = any
 type HasExactTags<B, T extends readonly string[]> = any
