02d8cecf0b3ce7642ef25fbe10b5a2b19724e1cc
许圣峰 <kricsleo@163.com>
2023-03-12 20:27:20 +0800

    00949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,12 @@
-type AnyOf<T extends readonly any[]> = any
+type IsTrue<T> = T extends number ? T extends 0 ? false : true
+  : T extends string ? T extends '' ? false : true
+  : T extends boolean ? T extends false ? false : true
+  : T extends unknown[] ? T extends [] ? false : true
+  : T extends Record<PropertyKey, unknown> ? keyof T extends never ? false : true
+  : false
+
+type c9 = IsTrue<[89]>
+
+type AnyOf<T extends readonly any[]> = T extends [infer A, ...infer Rest] 
+  ? IsTrue<A> extends true ? true : AnyOf<Rest>
+  : false
