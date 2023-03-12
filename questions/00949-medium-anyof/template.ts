eddec5a62821c03de70da217a168e5e343bb0f26
许圣峰 <kricsleo@163.com>
2023-03-12 20:29:27 +0800

    00949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,6 +1,6 @@
 type IsTrue<T> = T extends number ? T extends 0 ? false : true
   : T extends string ? T extends '' ? false : true
-  : T extends boolean ? T extends false ? false : true
+  : T extends boolean ? T extends true ? true : false
   : T extends unknown[] ? T extends [] ? false : true
   : T extends Record<PropertyKey, unknown> ? keyof T extends never ? false : true
   : false
