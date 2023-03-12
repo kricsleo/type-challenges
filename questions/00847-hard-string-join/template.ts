fdce025e9f4eec4ed705d1456f66ff773d3ee9b4
许圣峰 <kricsleo@163.com>
2023-03-12 17:16:50 +0800

    00847

--- questions/00847-hard-string-join/template.ts
+++ questions/00847-hard-string-join/template.ts
@@ -1,1 +1,8 @@
-declare function join(delimiter: any): (...parts: any[]) => any
+type JoinFn<S extends string, Params extends string[], R extends string = ''> = 
+  // todo: use 'as' after 'infer'
+  Params extends [infer P extends string, ...infer Rest extends string[]] 
+    ? Rest extends [] ? `${R}${P}` : JoinFn<S, Rest, `${R}${P}${S}`> 
+    : ''
+
+declare function join<T extends string>(delimiter: T): 
+  <Params extends string[]>(...parts: Params) => JoinFn<T, Params>
