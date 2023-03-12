7239ae4e5b42faa69bc5ced37b5348bc9d4e99d9
许圣峰 <kricsleo@163.com>
2023-03-12 19:59:45 +0800

    00898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,9 @@
-type Includes<T extends readonly any[], U> = any
+type MyEqual2<T, K> = 
+  // todo: !! https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-510924206
+  (<G>() => G extends T ? 1 : 2) extends
+  (<G>() => G extends K ? 1 : 2) ? true : false
+
+// loop over array
+type Includes<T extends readonly any[], U> = T extends [infer A, ...infer B]
+  ? MyEqual2<A, U> extends true ? true : Includes<B, U>
+  : false
