54a167c97f8410a12a2b69105d0993bea6a49c49
许圣峰 <kricsleo@163.com>
2023-03-13 13:02:45 +0800

    01097

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,1 +1,6 @@
-type IsUnion<T> = any
+// use array to wrap a type can prevent distributive extends
+// 'string | number extends string' will be distributed
+type IsUnion<T, B = T> = [T] extends [never] 
+  ? false : T extends any 
+    ? [B] extends [T] ? false : true 
+    : false
