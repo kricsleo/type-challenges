4b7c4a8f3cf7fd20c5dd4748e947ede0d657fbf2
许圣峰 <kricsleo@163.com>
2023-03-13 13:50:35 +0800

    01290

--- questions/01290-hard-pinia/template.ts
+++ questions/01290-hard-pinia/template.ts
@@ -1,1 +1,12 @@
-declare function defineStore(store: unknown): unknown
+
+type GettersReturn<Getters> = {
+  [key in keyof Getters]: Getters[key] extends () => infer R ? R : never
+}
+
+// much like vue.extend
+declare function defineStore<State, Getters, Actions>(store: {
+  id: string
+  state: () => State
+  getters: Readonly<Getters> & ThisType<Readonly<State> & GettersReturn<Getters> & Actions>
+  actions: Actions & ThisType<State & Readonly<GettersReturn<Getters>> & Actions>
+}): State & GettersReturn<Getters> & Actions
