a29ed0097aae88dfffe57df350685a02a3a35f13
许圣峰 <kricsleo@163.com>
2023-03-13 11:57:22 +0800

    01042

--- questions/01042-medium-isnever/template.ts
+++ questions/01042-medium-isnever/template.ts
@@ -1,1 +1,3 @@
-type IsNever<T> = any
+// never extends never  =>  never
+// @see https://github.com/microsoft/TypeScript/issues/31751#issuecomment-498526919
+type IsNever<T> = [T] extends [never] ? true : false
