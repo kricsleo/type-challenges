5b3a6ea321d9135edace040eebbc99d7883f8c63
许圣峰 <kricsleo@163.com>
2023-03-14 14:47:33 +0800

    02257

--- questions/02257-medium-minusone/template.ts
+++ questions/02257-medium-minusone/template.ts
@@ -1,1 +1,6 @@
-type MinusOne<T extends number> = any
+
+// leads to maximum deepth when number is too big( >= 1000)
+type MinusOne<T extends number, U extends any[] = []> = 
+  U['length'] extends T 
+    ? U extends [any, ...infer R] ? R['length'] : never
+    : MinusOne<T, [any, ...U]>
