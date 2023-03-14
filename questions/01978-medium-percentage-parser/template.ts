17dab692889db1931e7064cfa70e5eb597781e3b
许圣峰 <kricsleo@163.com>
2023-03-14 13:39:02 +0800

    01978

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,6 @@
-type PercentageParser<A extends string> = any
+type PercentageParser<A extends string> = 
+  A extends `${infer S1}${infer S2}` 
+    ? S1 extends '-' | '+' 
+      ? [S1, ...(S2 extends `${infer D}%` ? [D, '%'] : [S2, ''])]
+      : ['', ...(A extends `${infer D}%` ? [D, '%'] : [A, ''])]
+    : ['', '', '']
