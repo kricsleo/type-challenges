8f5900b9f365eea8c991c68636dc7c622a9c869f
许圣峰 <kricsleo@163.com>
2023-03-14 10:31:58 +0800

    01367

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,10 @@
-type RemoveIndexSignature<T> = any
+// index signature only refers to 'number' | 'string' | 'symbol' 
+// specific string like 'bar' is not an index signature
+type FilterPropertyKey<T> = string extends T ? never
+  : number extends T ? never 
+  : symbol extends T ? never
+  : T
+
+type RemoveIndexSignature<T> = {
+  [key in keyof T as FilterPropertyKey<key>]: T[key]
+}
