9c999ea324c51b4add0fbe1774629f5ae823beff
许圣峰 <kricsleo@163.com>
2023-03-15 13:21:30 +0800

    02822

--- questions/02822-hard-split/template.ts
+++ questions/02822-hard-split/template.ts
@@ -1,1 +1,6 @@
-type Split<S extends string, SEP extends string> = any
+type Split<S extends string, SEP extends string> = 
+  S extends `${infer S1}${SEP}${infer S2}` 
+    ? [S1, ...Split<S2, SEP>] 
+    : string extends S ? string[] 
+      : S extends SEP ? [] 
+        : [S]
