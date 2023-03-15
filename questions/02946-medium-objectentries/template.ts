f7b3bc9f141287aaa38bbabf82d34f4c943c275a
许圣峰 <kricsleo@163.com>
2023-03-15 13:40:30 +0800

    02946

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,10 @@
-type ObjectEntries<T> = any
+// require an optional 'undefined' leads to 'never'
+// Required3<{ key?: undefined }>   =>   { key: never }
+type Required3<T> = {
+  [key in keyof T]-?: T[key]
+}
+
+type ObjectEntries<T, key extends keyof T = keyof T> = 
+  key extends any 
+    ? [key, Required3<T>[key] extends never ? undefined : Required3<T>[key]] 
+    : never
