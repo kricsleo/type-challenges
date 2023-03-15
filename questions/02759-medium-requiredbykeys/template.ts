b658532955e3b79bb2994fcdb0f2647c510a7050
许圣峰 <kricsleo@163.com>
2023-03-15 11:43:27 +0800

    02759

--- questions/02759-medium-requiredbykeys/template.ts
+++ questions/02759-medium-requiredbykeys/template.ts
@@ -1,1 +1,11 @@
-type RequiredByKeys<T, K> = any
+type Copy4<T> = {
+  [key in keyof T]: T[key]
+}
+
+type MyOmit4<T, K extends keyof T> = {
+  [key in keyof T as key extends K ? never : key]: T[key]
+}
+
+type RequiredByKeys<T, K extends keyof T = keyof T> = Copy4<MyOmit4<T, K> & {
+  [key in K]-?: T[key]
+}>
