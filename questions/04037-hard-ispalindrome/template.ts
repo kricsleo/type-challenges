5542ae6f89802d75517c195adc4fa307a863efab
许圣峰 <kricsleo@163.com>
2023-03-16 17:00:17 +0800

    04037

--- questions/04037-hard-ispalindrome/template.ts
+++ questions/04037-hard-ispalindrome/template.ts
@@ -1,1 +1,7 @@
-type IsPalindrome<T> = any
+type ReverseString<T> = T extends `${infer S1}${infer S2}`
+  ? `${ReverseString<S2>}${S1}`
+  : ''
+
+type IsPalindrome<T extends string | number> = ReverseString<`${T}`> extends `${T}`
+  ? true 
+  : false
