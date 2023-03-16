d1af05ffc3b71dedd68865113aa58ef70f8846be
许圣峰 <kricsleo@163.com>
2023-03-16 14:13:32 +0800

    03326

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,9 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+// type b1 = 'a' | 'b'
+// type d1 = 'c' | 'd'
+// type e1 = `${b1}${d1}` // => 'ac' | 'ad' | 'bc' | 'bd'
+
+type Prefix<T extends any[], P extends string> = 
+  T extends [] ? '' : `${P}${T[number]}`
+
+type BEM<B extends string, E extends string[], M extends string[]> = 
+  `${B}${Prefix<E, '__'>}${Prefix<M, '--'>}`
