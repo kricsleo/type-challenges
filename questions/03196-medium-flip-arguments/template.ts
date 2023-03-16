5fd54261d4f07e7594bb57938e34be31840793a7
许圣峰 <kricsleo@163.com>
2023-03-16 11:50:50 +0800

    03196

--- questions/03196-medium-flip-arguments/template.ts
+++ questions/03196-medium-flip-arguments/template.ts
@@ -1,1 +1,6 @@
-type FlipArguments<T> = any
+type Reverse2<T extends any[]> = T extends [infer F, ...infer R] ? [...Reverse<R>, F] : []
+
+type FlipArguments<T extends Function> = 
+  T extends (...args: infer Args) => infer R 
+    ? (...args: Reverse2<Args>) => R
+    : never
