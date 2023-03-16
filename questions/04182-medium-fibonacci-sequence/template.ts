866fb614bcd31920d8d23eb94e149040ccc58f71
许圣峰 <kricsleo@163.com>
2023-03-16 17:12:03 +0800

    04182

--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,14 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  CurrentIndex extends any[] = ['🧮'],
+  Prev extends any[] = [],
+  Current extends any[] = ['🧮']
+> =
+  CurrentIndex['length'] extends T
+  ? Current['length']
+  : Fibonacci<
+      T,
+      [...CurrentIndex, '🧮'],
+      Current,
+      [...Prev, ...Current]
+    >;
