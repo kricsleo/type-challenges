a665d86a26dbc42d7c9901cb8e6563385accbcd8
许圣峰 <kricsleo@163.com>
2023-03-17 16:15:53 +0800

    04803

--- questions/04803-medium-trim-right/template.ts
+++ questions/04803-medium-trim-right/template.ts
@@ -1,1 +1,5 @@
-type TrimRight<S extends string> = any
+type TrimRight<S extends string> = 
+  S extends `${infer S1} ` ? TrimRight<S1>
+    : S extends `${infer S1}\n` ? TrimRight<S1>
+    : S extends `${infer S1}\t` ? TrimRight<S1>
+    : S
