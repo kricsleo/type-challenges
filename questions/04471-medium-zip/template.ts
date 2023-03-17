ae62d96c884a85469356ad9fd50b62615c62f37d
许圣峰 <kricsleo@163.com>
2023-03-17 10:28:32 +0800

    04471

--- questions/04471-medium-zip/template.ts
+++ questions/04471-medium-zip/template.ts
@@ -1,1 +1,6 @@
-type Zip<T, U> = any
+type Zip<T extends any[], U extends any[]> = 
+  T extends [infer T1, ...infer R1] 
+    ? U extends [infer U2, ...infer R2]
+      ? [[T1, U2], ...Zip<R1, R2>]
+      : []
+    : []
