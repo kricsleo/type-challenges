928347f500f31b1c9b0329dca33dba19a64a908f
许圣峰 <kricsleo@163.com>
2023-03-17 16:29:24 +0800

    05117

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,10 @@
-type Without<T, U> = any
+type Array2Union<T> = T extends [infer T1, ...infer Rest]
+  ? T1 | Array2Union<Rest>
+  : T
+
+type Without<T extends any[], U> = 
+  T extends [infer T1, ...infer Rest] 
+    ? T1 extends Array2Union<U>
+      ? Without<Rest, U>
+      : [T1, ...Without<Rest, U>]
+    : T
