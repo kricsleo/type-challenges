8dd13c9b7ae1c44e2dd1330c4cc3a72e87bd8bbd
许圣峰 <kricsleo@163.com>
2023-03-17 17:41:36 +0800

    05317

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,6 @@
-type LastIndexOf<T, U> = any
+type LastIndexOf<T extends any[], U> = 
+  T extends [...infer Rest, infer R]
+    ? MyEqual<U, R> extends true
+      ? Rest['length']
+      : LastIndexOf<Rest, U>
+    : -1
