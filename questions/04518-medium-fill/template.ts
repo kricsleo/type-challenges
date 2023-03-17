138d63d8ca08989d79bc24858a612aa89456a69b
许圣峰 <kricsleo@163.com>
2023-03-17 11:24:04 +0800

    04518

--- questions/04518-medium-fill/template.ts
+++ questions/04518-medium-fill/template.ts
@@ -3,4 +3,12 @@ type Fill<
   N,
   Start extends number = 0,
   End extends number = T['length'],
-> = any
+  Result extends any[] = [],
+  Index extends number = Start,
+> = Start extends End 
+  ? T
+  : Result['length'] extends Index 
+    ? Fill<T, N, Start, End, Result, Index extends Start ? End : Start>
+    : T extends [infer T1, ...infer Rest]
+      ? Fill<Rest, N, Start, End, [...Result, Index extends End ? N : T1], Index>
+    : Result
