adb1b38b4e25fdbce7dda28213289551e542df2c
许圣峰 <kricsleo@163.com>
2023-03-17 16:49:38 +0800

    05153

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,9 @@
-type IndexOf<T, U> = any
+type IndexOf<
+  T extends unknown[], 
+  U, 
+  Acc extends unknown[] = []
+> = T extends [infer First, ...infer Rest] 
+    ? MyEqual<First, U> extends true
+      ? Acc['length']
+      : IndexOf<Rest, U, [...Acc, 1]>
+    : -1
