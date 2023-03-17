756f4061f6d6b758d489d839119b8de2d9e845a3
许圣峰 <kricsleo@163.com>
2023-03-17 17:32:24 +0800

    05310

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,7 @@
-type Join<T, U> = any
+type Join<T extends (string | number)[], U extends string | number> = 
+  // use `extends` as `as` to infer a type
+  T extends [infer S1 extends string, ...infer Rest extends (string | number)[]]
+    ? Rest extends [] 
+      ? S1 
+      : `${S1}${U}${Join<Rest, U>}`
+    : ''
