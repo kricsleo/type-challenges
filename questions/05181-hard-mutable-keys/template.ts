0cfa75adde8cf2f45ebabd935bcaeb4f4dd6822e
许圣峰 <kricsleo@163.com>
2023-03-17 17:02:54 +0800

    05181

--- questions/05181-hard-mutable-keys/template.ts
+++ questions/05181-hard-mutable-keys/template.ts
@@ -1,1 +1,5 @@
-type MutableKeys<T> = any
+type MutableKeys<T> = 
+  keyof {
+    // if is readonly
+    [key in keyof T as (MyEqual<{ -readonly [key1 in key]: T[key] }, { [key1 in key]: T[key] }> extends true ? key : never)]: any
+  }
