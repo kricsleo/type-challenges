3984877433e1a567ec21cc6a269f5ead57d3d2c0
许圣峰 <kricsleo@163.com>
2023-03-18 18:26:38 +0800

    05423

--- questions/05423-hard-intersection/template.ts
+++ questions/05423-hard-intersection/template.ts
@@ -1,5 +1,8 @@
 type Array2Union2<T> = T extends any[] ? T[number] : T
 
+// 1 & unknown // => 1
+// 1 | unknown // => unknown
+
 type Intersection<T extends any[]> =
   T extends [infer Head, ...infer Tail]
     ? Array2Union2<Head> & Intersection<Tail>
