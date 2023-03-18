11b92d14ae78e68a44e0c6d99fbb358cfdd6b637
许圣峰 <kricsleo@163.com>
2023-03-18 21:18:15 +0800

    07258

--- questions/07258-hard-object-key-paths/template.ts
+++ questions/07258-hard-object-key-paths/template.ts
@@ -1,1 +1,15 @@
-type ObjectKeyPaths<T extends object> = any
+// object like '{}' only got 'string' keys
+// array object like '[]' got 'number' keys
+type GenNode<K extends string | number, IsRoot = true> = 
+  IsRoot extends true
+    ? `${K}` 
+    : `.${K}` | (K extends number ? `[${K}]` | `.[${K}]` : never)
+
+type ObjectKeyPaths<
+  T extends object,
+  IsRoot extends boolean = true,
+  K extends keyof T = keyof T
+> = 
+  K extends string | number
+    ? GenNode<K, IsRoot> | (T[K] extends object ? `${GenNode<K, IsRoot>}${ObjectKeyPaths<T[K], false>}` : never)
+    : never
