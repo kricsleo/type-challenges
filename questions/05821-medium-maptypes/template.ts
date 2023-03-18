37ebb7f22480bc169097def1e8834b0a56dafc95
许圣峰 <kricsleo@163.com>
2023-03-18 18:40:03 +0800

    05821

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,9 @@
-type MapTypes<T, R> = any
+type MapTypes<T, R extends { mapFrom: any; mapTo: any}> = {
+  [key in keyof T]: 
+    T[key] extends R['mapFrom'] 
+      // filter R from 'A | B' to the satisfied 'A'
+      ? R extends { mapFrom: T[key]} 
+        ? R['mapTo'] 
+        : never
+    : T[key]
+}
