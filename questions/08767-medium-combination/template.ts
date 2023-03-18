22f0ab86185acd8ea11f40663ad40a175fe4c2d1
许圣峰 <kricsleo@163.com>
2023-03-19 01:44:28 +0800

    08767

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -1,1 +1,7 @@
-type Combination<T extends string[]> = any
+// 'T' means a collection, 'U = T' limit 'U' to one of the collection
+type CombineString<T, U = T> =
+  U extends string
+    ? U | `${U} ${CombineString<Exclude<T, U>>}`
+    : ''
+
+type Combination<T extends string[]> = CombineString<T[number]>
--- questions/08767-medium-combination/test-cases.ts
+++ questions/08767-medium-combination/test-cases.ts
@@ -1,5 +1,5 @@
 import type { Equal, Expect } from '@type-challenges/utils'
-
+type c22= Combination<['foo', 'bar', 'baz']>
 type cases = [
   Expect<Equal<Combination<['foo', 'bar', 'baz']>,
   'foo' | 'bar' | 'baz' | 'foo bar' | 'foo bar baz' | 'foo baz' | 'foo baz bar' | 'bar foo' | 'bar foo baz' | 'bar baz' | 'bar baz foo' | 'baz foo' | 'baz foo bar' | 'baz bar' | 'baz bar foo'>>,
