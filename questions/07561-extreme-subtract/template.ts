5a7355a9df36d52f3fc89557be5fb7745cd4ff3e
许圣峰 <kricsleo@163.com>
2023-03-19 00:41:54 +0800

    07561

--- questions/07561-extreme-subtract/template.ts
+++ questions/07561-extreme-subtract/template.ts
@@ -1,2 +1,10 @@
+type Number2Array<T extends number, Acc extends any[] = []> =
+  Acc['length'] extends T 
+    ? Acc
+    : Number2Array<T, [...Acc, 1]>
+
 // M => minuend, S => subtrahend
-type Subtract<M extends number, S extends number> = any
+type Subtract<M extends number, S extends number> = 
+  Number2Array<M> extends [...Number2Array<S>, ...infer Tail]
+    ? Tail['length']
+    : never
