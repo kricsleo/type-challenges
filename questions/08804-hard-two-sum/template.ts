e68fbc6fa589b2da9ed2ab45accd9e4a3a9922d0
许圣峰 <kricsleo@163.com>
2023-03-19 15:21:51 +0800

    08804

--- questions/08804-hard-two-sum/template.ts
+++ questions/08804-hard-two-sum/template.ts
@@ -1,1 +1,14 @@
-type TwoSum<T extends number[], U extends number> = any
+type Number2Array2<T, Acc extends any[] = []> =
+  Acc['length'] extends T 
+    ? Acc
+    : Number2Array2<T, [...Acc, any]>
+
+type TwoSum<T extends any[], U extends number> = 
+  T extends [infer Head, ...infer Tail]
+    ? Number2Array2<U> extends [...Number2Array2<Head>, ...infer Tail2]
+      ? Tail2['length'] extends Tail[number]
+        ? true
+        : TwoSum<Tail, U>
+      : TwoSum<Tail, U>
+    : false
+
