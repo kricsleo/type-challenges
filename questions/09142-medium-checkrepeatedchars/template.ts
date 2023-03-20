771c7954b9cfcb91b099d1b29719a0fa5c95b466
许圣峰 <kricsleo@163.com>
2023-03-20 12:01:21 +0800

    09142

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,20 @@
-type CheckRepeatedChars<T extends string> = any
+// type Contain<T, K> = 
+//   T extends `${infer Head}${infer Tail}`
+//     ? Head extends K 
+//       ? true
+//       : Contain<Tail, K>
+//     : false
+
+// type CheckRepeatedChars3<T extends string> = 
+//   T extends `${infer Head}${infer Tail}`
+//     ? Contain<Tail, Head> extends true
+//       ? true
+//       : CheckRepeatedChars3<Tail>
+//     : false
+
+type CheckRepeatedChars<T extends string> = T extends `${infer First}${infer Rest}`
+  // Wow! A simple `string contains`!
+  ? Rest extends `${any}${First}${any}`
+    ? true
+    : CheckRepeatedChars<Rest>
+  : false
