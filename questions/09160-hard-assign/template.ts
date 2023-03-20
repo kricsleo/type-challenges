5378e55a73cd5d69e68cd93b33d1d53aafb2aaa0
许圣峰 <kricsleo@163.com>
2023-03-20 15:30:07 +0800

    09160

--- questions/09160-hard-assign/template.ts
+++ questions/09160-hard-assign/template.ts
@@ -1,1 +1,13 @@
-type Assign<T extends Record<string, unknown>, U> = any
+type AssignOne<Source, Target, K extends keyof (Source & Target) = keyof (Source & Target)> =
+  Target extends Record<string, unknown> ? {
+    [key in K]: key extends keyof Target 
+      ? Target[key] 
+      : key extends keyof Source 
+        ? Source[key]
+        : never
+  } : Source
+
+type Assign<T extends Record<string, unknown>, U> = 
+  U extends [infer Head, ...infer Tail]
+    ? Assign<AssignOne<T, Head>, Tail>
+    : T
