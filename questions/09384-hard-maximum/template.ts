e8d7243d7b2173c380ac6c9a00bad2b15f7e8847
许圣峰 <kricsleo@163.com>
2023-03-20 16:37:22 +0800

    09384

--- questions/09384-hard-maximum/template.ts
+++ questions/09384-hard-maximum/template.ts
@@ -1,1 +1,12 @@
-type Maximum<T extends any[]> = any
+type ReturnGreater<T extends number, U extends number, Acc extends unknown[] =[]> = T extends Acc['length']
+? U
+: U extends Acc['length']
+  ? T
+  : ReturnGreater<T, U, [...Acc, '']>
+
+// use 'extends' in 'infer' to limit 'infer' variable type 
+type Maximum<T extends unknown[]> = T extends [infer First extends number, infer Second extends number, ...infer Rest]
+? Rest extends []
+  ? ReturnGreater<First, Second>
+  : Maximum<[ReturnGreater<First, Second>, ...Rest]>
+: never
