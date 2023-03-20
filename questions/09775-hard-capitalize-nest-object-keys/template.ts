22fd3675fb55e95cdaecdc87ecc753b5d917db75
许圣峰 <kricsleo@163.com>
2023-03-20 16:48:49 +0800

    09775

--- questions/09775-hard-capitalize-nest-object-keys/template.ts
+++ questions/09775-hard-capitalize-nest-object-keys/template.ts
@@ -1,1 +1,13 @@
-type CapitalizeNestObjectKeys<T> = any
+type CapitalizeArray<T> =
+  T extends [infer Head, ...infer Tail]
+    ? [CapitalizeNestObjectKeys<Head>, ...CapitalizeArray<Tail>]
+    : T
+
+type CapitalizeNestObjectKeys<T> = 
+  T extends any[]
+    ? CapitalizeArray<T>
+    : T extends Record<string, any>
+      ? {
+      [key in keyof T as key extends string ? Capitalize<key> : never]: CapitalizeNestObjectKeys<T[key]>
+    } : T
+
