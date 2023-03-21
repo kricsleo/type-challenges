863fa734b7eb007aba253351b8d2598dc681ef39
许圣峰 <kricsleo@163.com>
2023-03-21 13:07:32 +0800

    15260

--- questions/15260-hard-tree-path-array/template.ts
+++ questions/15260-hard-tree-path-array/template.ts
@@ -1,1 +1,5 @@
-type Path<T> = any
+type Path<T, K extends keyof T = keyof T> = 
+  K extends never
+    ? never
+    // once again: [1, ...([2] | [])]       =>          [1] | [1, 2]
+    : [K, ...(T[K] extends Record<string, any> ? Path<T[K]> | [] : [])]
