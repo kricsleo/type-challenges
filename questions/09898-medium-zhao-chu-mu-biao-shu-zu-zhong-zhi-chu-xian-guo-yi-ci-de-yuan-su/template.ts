6d2f768f62253531ad510b475a48bdd48d1edd80
许圣峰 <kricsleo@163.com>
2023-03-21 10:46:08 +0800

    09898

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,8 @@
-type FindEles<T extends any[]> = any
+type FindEles<T extends any[], Acc extends any[] = [], Checked = never> =
+  T extends [infer Head, ...infer Tail]
+    ? Head extends Tail[number]
+      ? FindEles<Tail, Acc, Checked | Head>
+      : Head extends Checked
+        ? FindEles<Tail, Acc, Checked>
+        : FindEles<Tail, [...Acc, Head], Checked | Head>
+    : Acc
