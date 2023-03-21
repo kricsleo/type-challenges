577e97b3572736a5d13f5f65409584008f2e1c3e
许圣峰 <kricsleo@163.com>
2023-03-21 11:19:33 +0800

    13580

--- questions/13580-hard-replace-union/template.ts
+++ questions/13580-hard-replace-union/template.ts
@@ -1,1 +1,7 @@
-type UnionReplace<T, U extends [any, any][]> = any
+type UnionReplace<T, U extends [any, any][]> = 
+  U extends [infer Head extends [any, any],  ...infer Tail extends [any, any][]]
+    ? Head[0] extends T 
+      ? UnionReplace<Exclude<T, Head[0]> | Head[1], Tail>
+      : UnionReplace<T, Tail>
+    : T
+    
