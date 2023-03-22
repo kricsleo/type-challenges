41e9144f62633c3ce620a582728a72213bcc554f
许圣峰 <kricsleo@163.com>
2023-03-22 15:36:29 +0800

    19458

--- questions/19458-hard-snakecase/template.ts
+++ questions/19458-hard-snakecase/template.ts
@@ -1,1 +1,13 @@
-type SnakeCase<T> = any
+type IsUpperCase<T extends string> =
+  T extends Uppercase<T>
+    ? T extends Lowercase<T> 
+      ? false
+      : true
+    : false
+
+type SnakeCase<T extends string> = 
+  T extends `${infer Head}${infer Tail}`
+    ? IsUpperCase<Head> extends true
+      ? `_${Lowercase<Head>}${SnakeCase<Tail>}`
+      : `${Head}${SnakeCase<Tail>}`
+    : T
