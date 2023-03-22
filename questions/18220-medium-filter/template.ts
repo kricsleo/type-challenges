a2bdbc7d29587c592091e6467dd1138f3a31696c
许圣峰 <kricsleo@163.com>
2023-03-22 15:25:35 +0800

    18220

--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,6 @@
-type Filter<T extends any[], P> = []
+type Filter<T extends any[], P> =
+  T extends [infer Head, ...infer Tail]
+    ? Head extends P
+      ? [Head, ...Filter<Tail, P>]
+      : Filter<Tail, P>
+    : []
