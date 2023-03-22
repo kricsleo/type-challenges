2c8199f325723647e5cc616415d4ff3996c92844
许圣峰 <kricsleo@163.com>
2023-03-22 14:34:17 +0800

    16259

--- questions/16259-medium-to-primitive/template.ts
+++ questions/16259-medium-to-primitive/template.ts
@@ -1,1 +1,12 @@
-type ToPrimitive = any
+type ToPrimitive<T> = 
+  T extends Function 
+    ? Function
+    : T extends object ? {
+        [key in keyof T]: ToPrimitive<T[key]>
+      }
+      : T extends string ? string
+      : T extends number ? number 
+      : T extends boolean ? boolean
+      : T extends symbol ? symbol
+      : never
+  
